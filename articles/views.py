b5fd09124ee122486fde435bde85e39f4e2b00bd
Vero <veronika@mail.ru>
2024-12-17 14:52:22 +0200

    The context was added

--- articles/views.py
+++ articles/views.py
@@ -7,8 +7,7 @@ def articles_list(request):
     template = 'articles/news.html'
     context = {}
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
+    articles = Article.objects.all().order_by('-published_at')
+    context['object_list'] = articles
 
     return render(request, template, context)
